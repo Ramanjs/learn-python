49f007eaf66bf0dd2ee499b8b8117be69cdb1a78
Ramanjs <raman.s2662@gmail.com>
2021-11-18 00:36:38 +0530

    first commit

---
+++ basics/basics-1.py
@@ -0,0 +1,35 @@
+# Single line comments start with a hashtag
+
+""" Multiline comments
+    are often useful as 
+    well
+"""
+
+
+#################################################
+## Operators and Precedence
+#################################################
+
+(1 + 2)  # Paranthesis (1)
+4 ** 5   # Exponentiation (1)
+22 % 8   # Modulus (remainder) (2)
+10 * 2   # Multiplication (2)
+35 / 5   # Division (2)
+35 // 5  # Integer division (2)
+1 + 1    # Addition (3)
+8 - 1    # Sutraction (3)
+
+# TODO
+# Equality operators
+# Logical operators
+# Assignment operator
+
+
+#################################################
+## Data Types
+#################################################
+
+# Integers (int)
+# Floating point numbers (float)
+# Boolean 
+# Strings
