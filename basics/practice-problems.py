a8bace3f3138f3785ca882efcd383868b16e059d
Ramanjs <raman.s2662@gmail.com>
2021-11-20 00:26:17 +0530

    practice problems

--- basics/practice-problems.py
+++ basics/practice-problems.py
@@ -13,3 +13,46 @@ print(x)
 
 # 2 #
 
+a = 9 * 4 // 12 - (7 % 5 + 3)
+print(a)
+# Write answer below
+
+
+# 3 #
+
+b = (2 and 3) or (4 and 0)
+c = (7 or 0) and (1 or 6)
+d = (b == c)
+print(b)
+print(c)
+print(d)	
+# Write ans below
+
+
+
+
+# 4 #
+
+some_string = "Wow!, programming is way easier than I thought."
+other_string = "So far."
+final_string = some_string + " " + other_string
+print(final_string)
+# Write answer below
+
+
+
+# 5 #
+
+new_string = "what a day"
+other_string = "maybe"
+final_string = other_string[2] + other_string[1]
+last_character = new_string[len(new_string) - 1]
+print(final_string + last_character)
+# Write answer below
+
+
+# 6 #
+
+""" Write a program to take input user's first name and user's last name.
+    Then print full name of the user with a space between first and last names.
+"""
