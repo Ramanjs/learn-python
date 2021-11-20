4d170e4698ec2981441a087df634b5a036becd46
Ramanjs <raman.s2662@gmail.com>
2021-11-20 19:21:14 +0530

    if example

--- basics/if-condition.py
+++ basics/if-condition.py
@@ -1,4 +1,10 @@
-age = int(input("Enter age"))
-if (age > 10):
-	print("Raj is older than 10 years")
 # If the condition or expression inside the if statement evaluates to true then the if block is executed, other wise it is skipped
+
+age = int(input("Enter your age "))
+
+if (age >= 65):
+	print("You are a senior citizen")
+elif (age >= 18 and age < 65):
+	print("You are an adult")
+else:
+	print("You are a minor")
