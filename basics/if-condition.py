75b62b8549613239b011ef8c9ebca5127c53cc43
Ramanjs <raman.s2662@gmail.com>
2021-11-21 19:19:31 +0530

    if else

--- basics/if-condition.py
+++ basics/if-condition.py
@@ -1,10 +1,13 @@
 # If the condition or expression inside the if statement evaluates to true then the if block is executed, other wise it is skipped
 
-age = int(input("Enter your age "))
+# age = int(input("Enter your age "))
 
-if (age >= 65):
-	print("You are a senior citizen")
-elif (age >= 18 and age < 65):
-	print("You are an adult")
-else:
-	print("You are a minor")
+# if (age >= 65):
+# 	print("You are a senior citizen")
+# elif (age >= 18 and age < 65):
+# 	print("You are an adult")
+# else:
+# 	print("You are a minor")
+
+while (True):
+	print("infinite loop")
