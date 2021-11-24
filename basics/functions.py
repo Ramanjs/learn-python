acb615aa7b6c596165143c99bc7fb62f0a30b7bf
Ramanjs <raman.s2662@gmail.com>
2021-11-24 23:27:55 +0530

    formatting

--- basics/functions.py
+++ basics/functions.py
@@ -18,23 +18,25 @@ def myFunction(myVariable): # Parameter
 # ended the function
 
 # a function runs when it is called
-# myFunction("Raman") # calling the function
-# myFunction("Rishi") # calling the function with different argument
+myFunction("Raman") # calling the function
+myFunction("Rishi") # calling the function with different argument
+
+# To store the argument we pass in the function call, there should be 
+# a corresponding variable (parameter) to store the argument
 
 def add(num1, num2):
-	# return is kind of a break statement of functions
 	sum_var = num1 + num2
+	# return is kind of a break statement of functions
 	return sum_var
 
 
-# return_variable = add(4, 8)
-#print(return_value)
+return_value = add(4, 8)
+print(return_value) # -> 12
 
 def modulo(num1, num2):
 	print(num1 % num2)
 
-
-# modulo(4, 5)
+modulo(4, 5)
 
 
 def reverse_string(string_variable):
@@ -50,45 +52,39 @@ def reverse_string(string_variable):
 reverse1 = reverse_string("raman")
 reverse2 = reverse_string("rishi")
 reverse3 = reverse_string("parth")
-print(reverse1)
-print(reverse2)
-print(reverse3)
-
-
-
-
-
-# To store the argument we pass in the function call, there should be 
-# a corresponding variable to store the argument
+print(reverse1) # -> "namar"
+print(reverse2) # -> "ihsir"
+print(reverse3) # -> "htrap"
+# We can use the same function with different arguments to produce different results
 
 # Function is used to reduce repetion of code
 
-#print("hello world") # i am calling the print function
-
 # return values and return statement
-
 # When a function ends, it always returns a value
 # to return a value we can use the return statement
 # If we do not use the return statement then default value is None
-# None value
 
 
 # Local and global scope
-myName = "raman"
+# A variable defined inside a function has local scope. It means that it cannot be accessed outside that function
+# A global variable can be accessed anywhere
+myName = "raman" # myName is a global variable
 def add(num1, num2):
-	sum_var = num1 + num2
+	sum_var = num1 + num2 # sum_var is a local variable
 	print(myName)
 	return sum_var
 
 result = add(2, 3)
-# print(sum_var)
+# print(sum_var) # cannot access sum_var outside add function
+
 
 # Modules
 # We can import functions from other files
-from module import divide
+from module import divide # We need to tell python the file name from which we are importing
 result = divide(5, 6)
 print(result)
-import random
+
+import random # random is a built-in module
 
 random_integer = random.randint(1, 5)
 print(random_integer)
