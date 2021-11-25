2a60eeadf20508b6324c0fbe4d5b0155241b11aa
Ramanjs <raman.s2662@gmail.com>
2021-11-25 13:31:51 +0530

    practice lists

---
+++ basics/practice-problems-2.py
@@ -0,0 +1,38 @@
+# 1 #
+""" Write a program to print natural numbers from 1 to 25 """
+
+# 2 #
+""" Write a program to print the sum of first 10 natural numbers """
+
+# 3 #
+""" Write a program to print multiples of 3 from 1 to 45 """
+
+# 4 #
+""" Write a program to input a string from the user and print each character of the string in a new line (use for loop) """
+
+# 5 #
+""" Write a function which takes a string as an argument and returns the number of characters in that string """
+
+# 6 #
+places = ['delhi', 'goa', 'mumbai', 'ranchi', 'sikkim']
+# Write a line of code to insert 'nainital' before 'ranchi'
+
+
+# 7 #
+# Write a line of code to add 'bathinda' at the end of the same list
+
+
+# 8 #
+print(places[1:3])
+# Write output below
+
+
+# 9 #
+myVar = places.index('mumbai')
+print(myVar)
+# Write answer below
+
+
+# 10 #
+#### BONUS POINTS FOR SOLVING THIS QUESTION ####
+""" Write a program to print the number of characters in each place in the places list """
