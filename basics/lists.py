07585cf8a4a3a3fe4ac5d884dec5699bd3ec4594
Ramanjs <raman.s2662@gmail.com>
2021-11-25 11:10:46 +0530

    lists

--- basics/lists.py
+++ basics/lists.py
@@ -49,7 +49,60 @@ print(newList) # -> ['t', 'h', 'i', 's']
 myFriends.append("raj")
 print(myFriends)
 
-students = []
-for i in range(0, 50):
-	new_student = input("Enter student name ")
-	students.append(new_student)
+# changing list items
+
+myFriends[3] = "this"
+
+
+# adding lists
+
+print(myFriends + myNumbers)
+
+# shorthand updation
+num = 0
+num = num + 1
+
+num += 1 # num = num + 1
+num -= 1 # num = num - 1
+num *= 3 # num = num * 3
+num /= 5 # num = num / 5
+
+# for loop in lists
+
+for item in myFriends:
+	print(item)
+
+for i in range(0, len(myFriends), 1):
+	print(myFriends[i])
+
+
+string_var = "this"
+
+for charachter in string_var:
+	print(charachter)
+
+# index()
+# It searches for the argument in the given list and returns its index if found
+indexOfParth = myFriends.index("parth") 
+print(indexOfParth)
+
+# indexOf = myFriends.index("ram")
+# print(indexOf)
+
+# insert()
+# It inserts given value inside the list
+myFriends.insert(4, "harsh")
+print(myFriends)
+
+# remove()
+# It removes the given value from the list
+myFriends.remove("harsh")
+print(myFriends)
+
+# myFriends.remove("new")
+
+# sort(), arranging in ascending order
+myFriends.sort()
+print(myFriends)
+
+myFriends.sort(reverse = False)
