e3154851e14c995f82609b90a4cd4244dd36f43f
Ramanjs <raman.s2662@gmail.com>
2021-11-26 23:11:24 +0530

    finished dictionary

--- basics/dictionary.py
+++ basics/dictionary.py
@@ -1,6 +1,63 @@
-# Key-value pairs. Keys are like a lists indexes
+#################################################
+## Dictionary
+#################################################
+
+# Word: meaning
+# Key: value 
+# Data is stored in key-value pairs
+# Dict does not have indeces
+# Dicts are unordered
+# Data can be in any random order
+# To access our data we need its key
+
+myDict = { 
+	"mutable": "cannot be changed",
+ 	"tom M riddle": "Lord Voldemort",
+ 	452 : "four five two",
+	"myList": ["one", "two", "three"]
+}
+
+value = myDict["mutable"]
+print(value)
+print(len(myDict))
+
+print(myDict["myList"])
+
+# Key-value pairs. Keys are like a list indexes
+
 # Mutable
-# Unordered
+# one : 1
+# Adding new item into the dictionary
+myDict["one"] = 1
+
+# print(myDict)
+
+# Unordered -> no indexes, can exist in random order
+
 # keys(), values(), items()
+
+# keys()
+# keys() is a internal function of dictionary
+# to call internal functions we use dot operator
+myKeys = myDict.keys()
+print(myKeys)
+
+# values()
+
+myValues = myDict.values()
+print(myValues)
+
+# items()
+
+myItems = myDict.items()
+print(myItems)
+
+
+
+
 # get()
-# pprint()
+#print(myDict["random"])
+myValue = myDict.get("random")
+
+a = "one" in myDict
+print(a)
