20633216447c9e1479c9d9657a1e64867bed37a2
Ramanjs <raman.s2662@gmail.com>
2021-11-27 13:09:30 +0530

    added summary

---
+++ basics/summary.py
@@ -0,0 +1,26 @@
+###################
+# General Python
+###################
+
+# Data types
+# Operators
+# if else
+# while loops, for loops
+# functions
+# lists, tuples
+# dictionary
+# We can make complex programs
+# Rock paper scissors (if else, input(), while)
+
+###################
+# Further branching
+###################
+# File management (automating stuff)
+# Web Scraping (bots which can surf the internet)
+# SQL : 1 Week
+# Data analysis
+# numpy, pandas, scipy, matplotlib
+# NumPy - Numerical Python (lists)
+# Pandas - data analysis
+# SciPy - Scientific Python
+# MatPlotLib - Mathematical Plotting library
